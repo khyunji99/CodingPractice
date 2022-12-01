5f660801c61470b469de54cce5865f07addeb936
khyunji99 <117277248+khyunji99@users.noreply.github.com>
2022-12-01 12:05:31 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int angle) {
+        if(0 < angle && angle < 90) {
+            return 1;
+        } else if (angle == 90) {
+            return 2;
+        } else if (angle > 90 && angle < 180) {
+            return 3;
+        } else{
+            return 4;
+        }
+    }
+}
