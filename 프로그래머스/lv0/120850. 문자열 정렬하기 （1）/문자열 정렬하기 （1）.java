778a8a044b06153671556f49d88e77a2620eb3c3
khyunji99 <117277248+khyunji99@users.noreply.github.com>
2022-12-04 01:11:25 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.52 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.regex.Pattern;
+class Solution {
+    public int[] solution(String my_string) {
+        // 1. 문자열에서 숫자인 문자들 추출하기
+        // 2. 숫자인 문자들 숫자로 변환하여 숫자배열에 넣기
+        // 3. 숫자배열 정렬하기 
+        // * .length : 배열의 길이,크기
+        // * .length() : 문자열의 길이
+        my_string = my_string.replaceAll("[^0-9]", "");
+        String[] str = my_string.split("");
+        int[] answer = new int[str.length];
+        for(int i = 0; i < str.length; i++){
+            answer[i] = Integer.parseInt(str[i]);
+        }
+        
+        // for(String num : str){
+        //     boolean flag = Pattern.matches("[0-9]",num);
+        //     if(flag){
+        //         answer[i] = Integer.parseInt(num);
+        //         i++;
+        //     }
+        // }
+        
+        Arrays.sort(answer);
+        return answer;
+    }
+}
