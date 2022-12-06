9d4162f62bded1a0ef6cd6c9951f78f94c67b22f
khyunji99 <117277248+khyunji99@users.noreply.github.com>
2022-12-07 00:33:54 +0900

    [level 0] Title: 중복된 문자 제거, Time: 2.45 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,13 +1,35 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public String solution(String my_string) {
+        String[] strArr = my_string.split("");
+        List<String> strList = new ArrayList<String>(Arrays.asList(strArr));
+        
+        for(int i = 0; i < strList.size(); i++){
+            for(int j = i+1; j < strList.size(); j++){
+                if(strList.get(i).equals(strList.get(j))){
+                    strList.set(j, "");  // 중복되는 값이 있는 공간 빈공간으로 만들어버리기
+                }
+            }
+        }
         
         String answer = "";
         
-        for(int i = 0; i < my_string.length(); i++){
-            if(!answer.contains(String.valueOf(my_string.charAt(i)))){
-                answer += String.valueOf(my_string.charAt(i));
-            }
+        for(String word : strList){
+            answer += word;
         }
+        
+        
+//         두번째 푸는 방법
+//         String answer = "";
+        
+//         for(int i = 0; i < my_string.length(); i++){
+//             if(!answer.contains(String.valueOf(my_string.charAt(i)))){
+//                 answer += String.valueOf(my_string.charAt(i));
+//             }
+//         }
         
         
         return answer;
