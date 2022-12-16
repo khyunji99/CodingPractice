080eb526dd42e3342cdaf005c4b04687c91621de
khyunji99 <117277248+khyunji99@users.noreply.github.com>
2022-12-16 15:37:21 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.45 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,34 +1,13 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public long solution(String numbers) {
-        // hashMap 이용,,
-        Map<String, String> map = new HashMap<String,String>();
-        map.put("zero", "0");
-        map.put("one", "1");
-        map.put("two", "2");
-        map.put("three", "3");
-        map.put("four", "4");
-        map.put("five", "5");
-        map.put("six", "6");
-        map.put("seven", "7");
-        map.put("eight", "8");
-        map.put("nine", "9");
-        
-        
-        StringBuilder sb = new StringBuilder();
-        String answer = "";
-        
-        for(int i = 0; i < numbers.length(); i++){
-            sb.append(numbers.substring(i, i+1));
-            if(map.containsKey(sb.toString())){ //containsKey() : 주어진 key값이 있는지 확인 boolean리턴
-                answer += map.get(sb.toString()); // 해당하는 key값에 대한 value값 가져와서 문자열에 붙이기
-                
-                sb.setLength(0); // sb 초기화해서 처음부터 새로운 문자열 붙이기
+
+            String[] numbers_arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+            for (int i = 0; i < numbers_arr.length; i++) {
+                numbers = numbers.replaceAll(numbers_arr[i], String.valueOf(i));
             }
+
+            long answer = Long.parseLong(numbers);
+            return answer;
         }
-        
-        return Long.parseLong(answer);
-    }
 }
