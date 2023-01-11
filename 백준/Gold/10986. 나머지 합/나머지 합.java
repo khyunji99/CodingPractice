afdd3599d3931dae454872b1747ff55443073f29
khyunji99 <117277248+khyunji99@users.noreply.github.com>
2023-01-11 15:01:51 +0900

    [Gold III] Title: 나머지 합, Time: 516 ms, Memory: 120532 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,7 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // N개의 수 A1, A2, A3, ..., An이 주어졌을 때 연속된 부분의 합이 M으로 나누어떨어지는 구간의 개수를 구하는 프로그램을 작성하라.
         // 즉, Ai + ... + Aj (i<=j)의 합이 M으로 나누어떨어지는 (i,j) 쌍의 개수를 구하라.
 
@@ -20,20 +24,20 @@ public class Main {
         // C : 같은 나머지 값의 인덱스 카운트하는 배열
         // (즉, 업데이트한 합배열 S` 의 2,3번 인덱스가 나머지값 1로 같다. 이러면 C의 배열의 1번 인덱스(같은 나머지값을 C 배열의 인덱스로)를 +2 해주는 것이다.)
 
-
-
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         long[] S = new long[N];
         long[] C = new long[M]; // C 배열은 M의 나머지 값을 인덱스로 가지는 배열이기 때문에 M이 가지는 나머지의 개수는 0 ~ M-1 까지로 M 개이다.
         long answer = 0; // 우리가 원하는 경우의 수 값
 
 
         // 1. 합배열 S 만들기
-        S[0] = sc.nextInt(); // 맨 처음에 입력받는 원소
+        st = new StringTokenizer(br.readLine());
+        S[0] = Integer.parseInt(st.nextToken()); // 맨 처음에 입력받는 원소
         for (int i = 1; i < N; i++) {
-            S[i] = S[i-1] + sc.nextInt(); // 두번째부터 입력 받는 값으로 합배열을 만든다.
+            S[i] = S[i-1] + Integer.parseInt(st.nextToken()); // 두번째부터 입력 받는 값으로 합배열을 만든다.
         }
 
         // 2. 합배열 S 를 M 으로 나누고 난 나머지의 값에 해당하는 C 배열의 인덱스를 1씩 증가한다.
@@ -56,13 +60,14 @@ public class Main {
         //    ex) M = 3, C = [4, 2, 1]
         //    0번 인덱스의 값이 4 라는 말은 합배열의 원소를 M=3 으로 나머지연산 했을 때 나머지 값이 0인 애들이 4개가 된다는 말.
         //    그래서 나머지 값이 같은 4개 중에서 2개를 뽑는 경우의 수 4C2 의 값인 6을 answer 에 더해준다.
-        
+
         for (int i = 0; i < M; i++) {
             if(C[i] > 1)
                 answer += C[i] * (C[i] - 1) / 2; // 조합의 공식을 간단하게 나타내면 n(n-1)/2 가 된다,,,
         }
-        
+
         System.out.print(answer);
+
 
     }
 }
