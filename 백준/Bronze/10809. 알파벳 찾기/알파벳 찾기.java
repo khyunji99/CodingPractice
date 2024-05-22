d2c2bb4418c009b7f7ef473ebe1cb7e8aa46704b
Gil hyun ji <117277248+khyunji99@users.noreply.github.com>
2024-05-22 15:14:39 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -3,11 +3,13 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         String str = br.readLine();
-        String[] alp = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
-        for (int i = 0; i < alp.length; i++) {
-            System.out.print(str.indexOf(alp[i]) + " ");
+        for (char c = 'a'; c <= 'z'; c++) {
+            sb.append(str.indexOf(c) + " ");
         }
+        
+        System.out.println(sb);
     }
 }
