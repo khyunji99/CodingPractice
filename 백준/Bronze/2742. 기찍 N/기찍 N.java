372afe92c2366427135985d5a93c03b16d45f093
Gil hyun ji <117277248+khyunji99@users.noreply.github.com>
2024-05-22 16:56:18 +0900

    [Bronze IV] Title: 기찍 N, Time: 292 ms, Memory: 22492 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -1,12 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-
-        for (int i = num; i > 0; i--) {
-            System.out.println(i);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(N > 0) {
+            sb.append(N+"\n");
+            N--;
         }
+        
+        System.out.println(sb);
     }
 }
