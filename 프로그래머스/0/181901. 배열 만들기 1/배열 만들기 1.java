6b14d8faa47b7f8ae65ba3e3f168fa886b322d22
Gil hyun ji <117277248+khyunji99@users.noreply.github.com>
2024-05-30 14:02:15 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[n/k];
+        for (int i = 1; i <= n/k; i++) {
+            answer[i-1] = k * i;
+        }
+        return answer;
+    }
+}
