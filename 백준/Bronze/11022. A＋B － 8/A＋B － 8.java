146839f494aa1bf6e327089ce4af2fd54c308c9a
Gil hyun ji <117277248+khyunji99@users.noreply.github.com>
2024-05-30 16:49:49 +0900

    [Bronze V] Title: A+B - 8, Time: 124 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,22 +1,22 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-
+ 
 public class Main {
-    public static void main(String args[]) throws IOException {
-
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-
-        int TC = Integer.parseInt(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= TC; i++) {
-            String str = br.readLine();
-            sb.append("Case #").append(i).append(": ").append(str.charAt(0)-'0')
-                    .append(" + ").append(str.charAt(2)-'0').append(" = ")
-                    .append((str.charAt(0)-'0')+(str.charAt(2)-'0'))
-                    .append('\n');
-        }
-        System.out.println(sb);
-    }
+	public static void main(String args[]) throws IOException {
+ 
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		int TC = Integer.parseInt(br.readLine());
+ 
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= TC; i++) {
+			String str = br.readLine();
+			int A = str.charAt(0)-'0';
+			int B = str.charAt(2)-'0';
+			sb.append("Case #").append(i).append(": ").append(A)
+			.append(" + ").append(B).append(" = ").append(A+B).append('\n');
+		}
+		System.out.println(sb);
+	}
 }
