2362c02cae34775c1dc34044a610f8aab5970d49
Gil hyun ji <117277248+khyunji99@users.noreply.github.com>
2024-05-31 13:07:07 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 10.72 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 겹쳐쓰기 - 181943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181943?language=java) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 15.06 ms
+메모리: 74.1 MB, 시간: 10.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 31일 13:06:07
+2024년 05월 31일 13:07:06
 
 ### 문제 설명
 
--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        int l1 = my_string.length();
-        int l2 = overwrite_string.length();
-        return my_string.substring(0, s) + overwrite_string + my_string.substring(l2+s, l1);
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+        return before + overwrite_string + after;
     }
 }
