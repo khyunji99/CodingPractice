2de829095ed9203291ae8f123728f71abc123257
Gil hyun ji <117277248+khyunji99@users.noreply.github.com>
2024-06-24 21:16:54 +0900

    [level 0] Title: 수 조작하기 2, Time: 5.98 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < numLog.length - 1; i++) {
+            int d = numLog[i+1] - numLog[i];
+            
+            switch (d) {
+                case 1 -> {
+                    answer.append("w");
+                    break;
+                }
+                case -1 -> {
+                    answer.append("s");
+                    break;
+                }
+                case 10 -> {
+                    answer.append("d");
+                    break;
+                }
+                default -> {
+                    answer.append("a");
+                    break;
+                }
+            }
+        }
+        return answer.toString();
+    }
+}
